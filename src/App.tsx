4549fcbc6e964c28cc4f2fad61ef3da9ed326995
lucasgranjense23 <23.lucasdoliveira@gmail.com>
2023-06-20 09:33:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+
+      <div className="h-[800px] w-full text-3xl flex flex-col text-cyan-500 items-center justify-center">
+        <div className="font-semibold">T T T</div>
+        <div>Template Typescript + Tailwindcss</div>
+        <div className="text-white">SANGATI BERGA</div>
+      </div>
+  
+  );
+}
